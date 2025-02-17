fc957a6ee54aa7d43959253ede626a7e901c75f0
UmarVCET <umar.s2409256109@vcet.edu.in>
2025-02-17 14:43:09 +0530

    Update exp4 (68).py

--- exp4 (68).py
+++ exp4 (68).py
@@ -1,82 +1,44 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": 1,
-   "metadata": {},
-   "outputs": [
-    {
-     "name": "stdout",
-     "output_type": "stream",
-     "text": [
-      "Content before appending:\n",
-      " This is the first line.\n",
-      "This is the second line.\n",
-      "\n",
-      "Content after appending:\n",
-      " This is the first line.\n",
-      "This is the second line.\n",
-      "This is the appended line.\n",
-      "\n"
-     ]
-    }
-   ],
-   "source": [
-    "from contextlib import contextmanager\n",
-    "\n",
-    "@contextmanager\n",
-    "def genericFileFunction(filename, method):\n",
-    " \n",
-    "    file = open(filename, method)\n",
-    "    try:\n",
-    "\n",
-    "        yield file\n",
-    "    finally:\n",
-    "      \n",
-    "        file.close()\n",
-    "\n",
-    "if __name__ == '__main__':\n",
-    "    filename = 'file.txt'\n",
-    "    \n",
-    "\n",
-    "    with genericFileFunction(filename, 'w') as file:\n",
-    "        file.write(\"This is the first line.\\n\")\n",
-    "        file.write(\"This is the second line.\\n\")\n",
-    "    \n",
-    "    \n",
-    "    with genericFileFunction(filename, 'r') as file:\n",
-    "        content_before_append = file.read()\n",
-    "   \n",
-    "    with genericFileFunction(filename, 'a') as file:\n",
-    "        file.write(\"This is the appended line.\\n\")\n",
-    "    \n",
-    "    with genericFileFunction(filename, 'r') as file:\n",
-    "        content_after_append = file.read()\n",
-    "\n",
-    "    print(\"Content before appending:\\n\", content_before_append)\n",
-    "    print(\"Content after appending:\\n\", content_after_append)\n"
-   ]
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.12.0"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 2
-}
+# Remove the pass and call the function to perform write.
+    # Read the file that was created while writing.
+    # Make the changes in the file by appending some material.
+    # Again read the same file.
+
+'''
+Print all the output of the function.
+For better representation, please use jupyter.
+Will also aid in easy maintanence of files for submissions.
+'''
+
+from contextlib import contextmanager
+
+@contextmanager
+def genericFileFunction (filename, method):
+    file = open (filename, method)
+    yield file
+    file.close ()
+
+if __name__ == '__main__':
+    try:
+        with genericFileFunction("trial.txt","w") as f:
+            f.write("Namaste, I am UmarShaikh.")
+            print("\nWriting complete!")
+
+        with genericFileFunction("trial.txt","r") as f:
+            print(f.read())
+
+        with genericFileFunction("trial.txt","a") as f:
+            f.write(" I am a student.")
+            print("\nAppending complete!")
+
+        with genericFileFunction("trial.txt","r") as f:
+            print(f.read())
+
+    except Exception as e:
+        print(e)
+
+     
+Writing complete!
+Namaste, I am UmarShaikh.
+
+Appending complete!
+Namaste, I am UmarShaikh. I am a student.
